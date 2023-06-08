76a1fc61dfaff8047d8dbfb6a8dd2953983b736f
m.daffa.akbar <daffaraihan03@gmail.com>
2023-06-08 10:13:51 +0700

    fix: incorrect import

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from '../docs/swagger.json'
 import { AppRouter } from './routes/routes'
 
 const app = express()
@@ -7,6 +9,8 @@ const app = express()
 app.use(express.json())
 
 app.use(morgan('dev'))
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(AppRouter)
 

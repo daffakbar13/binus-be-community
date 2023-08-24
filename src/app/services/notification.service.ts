90aa8de380c9c368f8a76070d7f7260aed8b6f94
m.daffa.akbar <daffaraihan03@gmail.com>
2023-08-24 12:22:29 +0700

    refactor: catch error axios

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -56,7 +56,7 @@ export namespace NotificationService {
       )
       return result
     } catch (err) {
-      LoggingService.Error(req, Constant.ERR_SUPERAPPS_SERVICE, err)
+      LoggingService.Error(req, Constant.ERR_SUPERAPPS_SERVICE, JSON.stringify(err))
       return baseResponse('InternalServerError')
     }
   }
